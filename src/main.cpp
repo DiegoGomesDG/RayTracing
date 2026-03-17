73b0242ee57915a6e8de161a9a35ff29c36594b5
Diego Gomes <diego.gomes@edu.bme.hu>
2026-03-17 20:05:08 +0100

    Coloring based on normals

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,19 @@
 #include "ray.h"
 #include "vec3.h"
 
-bool hit_sphere(const point3 &center, double radius, const ray &ray) {
+float hit_sphere(const point3 &center, float radius, const ray &ray) {
     vec3 oc = center - ray.origin();
     auto a = dot(ray.direction(), ray.direction());
     auto b = -2.0f * dot(ray.direction(), oc);
     auto c = dot(oc, oc) - radius * radius;
 
     auto discr = b*b - 4 * a * c;
-    return discr >= 0;
+
+    if (discr < 0) {
+        return -1.0f;
+    } else {
+        return (-b - std::sqrt(discr)) / (2.0f*a);
+    }
 }
 
 color ray_color(const ray &r) {
@@ -21,8 +26,11 @@ color ray_color(const ray &r) {
     color blue(0.5, 0.7, 1.0);
     color red(1.0, 0.0, 0.0);
 
-    if (hit_sphere(point3(0, 0, -1), 0.5, r))
-        return red;
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
 
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5f * (unit_direction.y() + 1.0f);
