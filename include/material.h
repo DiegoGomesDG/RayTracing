43be614e017f918ee61aab20be620080dfe8c8ad
Diego Gomes <diego.gomes@edu.bme.hu>
2026-03-26 13:22:59 +0100

    Total internal reflection

--- include/material.h
+++ include/material.h
@@ -63,6 +63,18 @@ public:
         const double ri = rec.front_face ? (1.0/refraction_index) : refraction_index;
 
         const vec3 unit_direction = unit_vector(r_in.direction());
+        real cos_theta = std::fmin(dot(-unit_direction, rec.normal), 1.0);
+        real sin_theta = std::sqrt(1.0 - cos_theta * cos_theta);
+
+        bool cannot_refract = ri * sin_theta > 1.0;
+        vec3 direction;
+
+        if (cannot_refract)
+            direction = reflect(unit_direction, rec.normal);
+        else
+            direction = refract(unit_direction, rec.normal, ri);
+
+        scattered = ray(rec.p, direction);
         const vec3 refracted = refract(unit_direction, rec.normal, ri);
 
         scattered = ray(rec.p, refracted);
